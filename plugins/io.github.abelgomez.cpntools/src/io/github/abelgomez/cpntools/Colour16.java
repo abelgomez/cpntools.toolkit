bd1a208bc74d6069fa8979b1a6d31e21665e7417
Abel Gómez <abgolla@gmail.com>
2021-11-11 22:02:27 +0100

    Update license (EPLv1->EPLv2) and copyright notice

--- plugins/io.github.abelgomez.cpntools/src/io/github/abelgomez/cpntools/Colour16.java
+++ plugins/io.github.abelgomez.cpntools/src/io/github/abelgomez/cpntools/Colour16.java
@@ -1,13 +1,16 @@
-/**
- * Copyright (c) 2016 Abel Gómez.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+/*******************************************************************************
+ * Copyright (c) 2021 Abel Gómez.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
  * Contributors:
- *     Abel Gómez - initial API and implementation
- */
+ *     Abel Gómez (abgolla@gmail.com) - initial API and implementation
+ *******************************************************************************/
 package io.github.abelgomez.cpntools;
 
 import java.lang.String;
